e5eaf4f7bba9515964923d5c224f8c52b0851ced
lbr0452000 <lbr0452000@naver.com>
2018-06-22 16:19:24 +0900

    background

---
+++ JoonTarot/PickCard/MainPageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainPageViewController.swift
+//  JoonTarot
+//
+//  Created by Harrison on 2018. 6. 22..
+//  Copyright © 2018년 Harrison. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    }
+}
